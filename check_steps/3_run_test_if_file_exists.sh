2f48b4ca26d5d5dfd4ec145fa70cde9dabf30ceb
Dmitry Eremin <mitya1705@gmail.com>
2024-03-04 04:50:11 +0300

    npx install deps

--- check_steps/3_run_test_if_file_exists.sh
+++ check_steps/3_run_test_if_file_exists.sh
@@ -3,7 +3,8 @@ TEST=$2
 if [ -f $FILE ]; then
    echo "File exists."
    npm install
-   npx playwright install --silent && npx playwright install-deps chromium
+   npx playwright install --silent 
+   npx playwright install-deps chromium
    npx playwright test -g "$TEST"
 else
    echo "File $FILE does not exist."
