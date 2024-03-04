4ed385fb16d887c863a3daa980709c174ccc18fc
EreminD <mitya1705@gmail.com>
2024-03-04 05:00:04 +0300

    Update 3_run_test_if_file_exists.sh

--- check_steps/3_run_test_if_file_exists.sh
+++ check_steps/3_run_test_if_file_exists.sh
@@ -2,9 +2,8 @@ FILE=$1
 TEST=$2
 if [ -f $FILE ]; then
    echo "File exists."
-   npm install
-   npx playwright install --silent 
-   npx playwright install-deps chromium
+   npm install --silent 
+   npx playwright install && npx playwright install-deps chromium
    npx playwright test -g "$TEST"
 else
    echo "File $FILE does not exist."
