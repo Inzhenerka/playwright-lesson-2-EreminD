0536f6cbfbfb0a1ce2aab54c3134a32e1ced90b8
EreminD <mitya1705@gmail.com>
2024-03-04 04:56:39 +0300

    Update lesson2_click.spec.js

--- tests/lesson2_click.spec.js
+++ tests/lesson2_click.spec.js
@@ -1,1 +1,11 @@
+const { test, expect } = require('@playwright/test');
 
+test('click test', async ({ page }) => {
+  await page.goto('http://uitestingplayground.com/click');
+    
+  // >>>>> дополнить код здесь
+  await page.locator("#badButton").click();
+  // <<<<<
+    
+  expect(await page.locator("#badButton").getAttribute("class")).toMatch(/btn-success/);
+});
